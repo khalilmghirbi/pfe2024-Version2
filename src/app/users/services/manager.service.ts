f18284f814cf00a89765763a039610eb121e4bba
JJ6488 <amine.mghirbi@external.engie.com>
2024-10-20 15:10:53 +0200

    add update

---
+++ src/app/users/services/manager.service.ts
@@ -0,0 +1,109 @@
+import { Injectable } from '@angular/core';
+import { delay, Observable, of } from 'rxjs';
+import { User } from '../models/user';
+import { UpdateUserDialogComponent } from '../update-user-dialog/update-user-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ManagerService {
+  private managerNames: string[] = [
+    'John Doe',
+    'Jane Smith',
+    'Alice Johnson',
+    'Robert Brown',
+    'Emily Davis',
+    'Michael Wilson',
+    'Sarah Miller',
+    'David Anderson',
+    'Laura Thomas',
+    'James Jackson',
+  ];
+
+  private emails: string[] = [
+    'john@example.com',
+    'jane@example.com',
+    'alice@example.com',
+    'robert@example.com',
+    'emily@example.com',
+    'michael@example.com',
+    'sarah@example.com',
+    'david@example.com',
+    'laura@example.com',
+    'james@example.com',
+  ];
+
+  private phones: string[] = [
+    '123-456-7890',
+    '987-654-3210',
+    '555-555-5555',
+    '444-444-4444',
+    '333-333-3333',
+    '222-222-2222',
+    '111-111-1111',
+    '666-666-6666',
+    '777-777-7777',
+    '888-888-8888',
+  ];
+
+  private hospitals: string[] = [
+    'Mayo Clinic',
+    'Cleveland Clinic',
+    'Johns Hopkins Hospital',
+    'Massachusetts General Hospital',
+    'UCLA Medical Center',
+    'Cedars-Sinai Medical Center',
+    'NewYork-Presbyterian Hospital',
+    'UCSF Medical Center',
+    'Northwestern Memorial Hospital',
+    'NYU Langone Hospitals',
+  ];
+
+  private countries: string[] = [
+    'United States',
+    'Canada',
+    'United Kingdom',
+    'Australia',
+    'Germany',
+    'France',
+    'Italy',
+    'Spain',
+    'Japan',
+    'China',
+  ];
+
+  constructor(public dialog: MatDialog) {}
+
+  getManagers(): Observable<User[]> {
+    const users: User[] = Array.from({ length: 13 }, () => ({
+      id: Math.floor(Math.random() * 101), // Random string
+      name: this.getRandomItem(this.managerNames),
+      email: this.getRandomItem(this.emails),
+      phone: this.getRandomItem(this.phones),
+      hopital: this.getRandomItem(this.hospitals),
+      country: this.getRandomItem(this.countries),
+    }));
+
+    return of(users).pipe(delay(1000));
+  }
+
+  removeManager(id: number) {
+    console.log('Removing manager with id:', id);
+    return of({}).pipe(delay(1000));
+  }
+
+  updateManager(id: number, user: User) {
+    const dialogConfig = {
+      minWidth: '300px', // Set minimum width
+      width: '50vw', // Set width to 80% of the viewport width
+      maxWidth: '600px', // Set maximum width
+      data: user,
+    };
+    this.dialog.open(UpdateUserDialogComponent, dialogConfig);
+  }
+
+  private getRandomItem<T>(array: T[]): T {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+}
---
+++ src/app/users/users-table/users-table.component.ts
@@ -0,0 +1,62 @@
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { User } from '../models/user';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { ManagerService } from '../services/manager.service';
+import {
+  Subscription,
+} from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
+
+@Component({
+  selector: 'app-users-table',
+  templateUrl: './users-table.component.html',
+  styleUrls: ['./users-table.component.scss'],
+})
+export class UsersTableComponent implements OnInit, OnDestroy, AfterViewInit {
+  displayedColumns: string[] = [
+    'Name',
+    'Email',
+    'Phone',
+    'Hospital',
+    'Country',
+    'Actions',
+  ];
+  dataSource = new MatTableDataSource<User>();
+
+  resultsLength = 0;
+  isLoadingResults = true;
+  isRateLimitReached = false;
+  subscription!: Subscription;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(public managerService: ManagerService) {}
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  ngOnInit() {
+    this.subscription = this.managerService.getManagers().subscribe((data) => {
+      this.dataSource.data = data;
+    });
+  }
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  edit(user: User) {
+    this.managerService.updateManager(user.id, user);
+  }
+
+  remove(user: User) {
+    this.managerService.removeManager(user.id).subscribe();
+  }
+}
